516da56607e3f7fbf1bcfcae9ae1c2cf29906944
chithi <chithi@chithigun23.ip6.superloop.au>
2026-09-04 02:49:22 +1000

    Render complete

--- Render/ENCODE_MP4.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Turn the rendered PNG sequence into an MP4 using Blender's built-in ffmpeg.
-set BLENDER=
-if exist "C:\Program Files\Blender Foundation\Blender 5.2\blender.exe" set BLENDER=C:\Program Files\Blender Foundation\Blender 5.2\blender.exe
-if "%BLENDER%"=="" for /d %%D in ("C:\Program Files\Blender Foundation\Blender *") do set BLENDER=%%D\blender.exe
-"%BLENDER%" --background --python "%~dp0encode.py"
-pause
